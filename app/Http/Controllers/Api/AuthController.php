34b4fe1cf5c1779562fbb41bbc5c301bdf775efd
Adam Azmi <adam31azmi@gmail.com>
2024-06-13 13:35:29 +0200

    Fix response code in registration.

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -23,7 +23,7 @@ class AuthController extends Controller
         ]);
         
         if ($validator->fails()) {
-            return response()->json([$validator->errors(), 422]);
+            return response()->json([$validator->errors()], 422);
         }
         
         $user = User::create([
